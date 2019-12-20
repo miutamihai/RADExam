03323a236874aa614eaa8822a0fa69929485ab2f
Mihai Miuta <S00201405@mail.itsligo.ie>
2019-12-20 12:44:15 +0000

    s00201405 part1f

--- Rad301Semester1fe2019.MVC/Controllers/LibrarianController.cs
+++ Rad301Semester1fe2019.MVC/Controllers/LibrarianController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Rad301Semester1fe2019.MVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -22,6 +23,25 @@ namespace Rad301Semester1fe2019.MVC.Controllers
         public ActionResult ListOverdueBooks()
         {
             return View(get_general_overdues());
+        }
+        public ActionResult LoanBook()
+        {
+            return View(new CreateLoanModel());
+        }
+        [HttpPost]
+        public ActionResult Create(CreateLoanModel loan)
+        {
+            var context = new BusinessDomain.Classes.Context();
+            int memberID = context.Members.ToList().Find(member => member.FirstName == loan.FirstName && member.LastName == loan.LastName).MemberID;
+            int bookID = context.Books.ToList().Find(book => book.Title == loan.BookTitle).BookID;
+            context.Loans.Add(new BusinessDomain.Classes.Classes.Loan
+            {
+                MemberID = memberID,
+                BookID = bookID,
+                LoanIssueDate = DateTime.Today
+            });
+            context.SaveChanges();
+            return RedirectToAction("LoanBook");
         }
     }
 }
---
+++ Rad301Semester1fe2019.MVC/Models/CreateLoanModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rad301Semester1fe2019.MVC.Models
+{
+    public class CreateLoanModel
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BookTitle { get; set; }
+    }
+}
